01b7ef722de69c300e6158c6a45ad9691757029c
jgretz <jgretz@truefitsolutions.com>
2016-11-17 12:45:55 -0500

    complete lab3

--- src/components/heroes.js
+++ src/components/heroes.js
@@ -11,17 +11,40 @@ export default class Heroes extends Component {
     ];
   }
 
+  renderInput() {
+    return (
+      <div>
+        <input /><button>Add</button>
+      </div>
+    );
+  }
+
+  renderList(items) {
+    return (
+      <ol>
+        { items.map((item, index) => this.renderItem(item, index)) }
+      </ol>
+    );
+  }
+
+  renderItem(item, index) {
+    return (
+      <li key={index}>
+        <span className="title">Hero { index }: </span>
+        <span>{ item }</span>
+      </li>
+    );
+  }
+
+
   render() {
     const { items } = this;
 
     return (
       <div>
         <h2>Heroes</h2>
-        <ol>
-          <li>{ `Hero 0: ${items[0]}` }</li>
-          <li>{ `Hero 1: ${items[1]}` }</li>
-          <li>{ `Hero 2: ${items[2]}` }</li>
-        </ol>
+        { this.renderInput() }
+        { this.renderList(items) }
       </div>
     );
   }
