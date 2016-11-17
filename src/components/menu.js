8edd973cb30e5e9d4a0bbc8a6255158d12f4b359
jgretz <jgretz@truefitsolutions.com>
2016-11-17 14:10:50 -0500

    finish lab2

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Menu = () =>
 (
   <div>
-    <span>Heroes</span>
-    <span>Comics</span>
-    <span>Creators</span>
-    <span>Stories</span>
+    <Link to="/heroes">Heroes</Link>
+    <Link to="/comics">Comics</Link>
+    <Link to="/creators">Creators</Link>
+    <Link to="/stories">Stories</Link>
   </div>
 );
 
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,19 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 import Heroes from './components/heroes';
+import Comics from './components/comics';
+import Creators from './components/creators';
+import Stories from './components/stories';
 import NotFoundPage from './components/not_found';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Heroes}/>
+    <Route path="heroes" component={Heroes}/>
+    <Route path="heroes/:nameStartsWith" component={Heroes}/>
+    <Route path="comics" component={Comics}/>
+    <Route path="creators" component={Creators}/>
+    <Route path="stories" component={Stories}/>
     <Route path="*" component={NotFoundPage}/>
   </Route>
 );
