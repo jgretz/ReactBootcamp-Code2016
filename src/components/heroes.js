d3344ee9f773b191c506732b0cfafbd32c8024e3
jgretz <jgretz@truefitsolutions.com>
2016-11-17 12:43:59 -0500

    complete lab 2

--- src/components/heroes.js
+++ src/components/heroes.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Heroes = () =>
-(
-  <div>
-    <h2>Heroes</h2>
-    <ol>
-      <li>Wolverine</li>
-      <li>Storm</li>
-      <li>Wonderboy</li>
-    </ol>
-  </div>
-);
+export default class Heroes extends Component {
+  constructor(props) {
+    super(props);
 
-export default Heroes;
+    this.items = [
+      'Wolverine',
+      'Storm',
+      'Wonderboy'
+    ];
+  }
+
+  render() {
+    const { items } = this;
+
+    return (
+      <div>
+        <h2>Heroes</h2>
+        <ol>
+          <li>{ `Hero 0: ${items[0]}` }</li>
+          <li>{ `Hero 1: ${items[1]}` }</li>
+          <li>{ `Hero 2: ${items[2]}` }</li>
+        </ol>
+      </div>
+    );
+  }
+}
